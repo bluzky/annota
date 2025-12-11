424579a938a1220009f3b195a79a599a16debd40
Always blue <bluesky.1289@gmail.com>
2025-12-11 15:47:34 +0700

    Initial Commit

---
+++ texttoolTests/texttoolTests.swift
@@ -0,0 +1,17 @@
+//
+//  texttoolTests.swift
+//  texttoolTests
+//
+//  Created by Flex on 12/11/25.
+//
+
+import Testing
+@testable import texttool
+
+struct texttoolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
